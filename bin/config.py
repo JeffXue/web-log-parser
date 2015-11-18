bef6608593a71dc90bdde31ab07bf28888eda4a7
Jeff Xue <contactmexkj@163.com>
2015-11-18 15:17:09 +0800

    Update config.py
    修改配置文件相关名称

--- bin/config.py
+++ bin/config.py
@@ -11,15 +11,15 @@ class Config():
         with open(config_file, 'r') as cfg_file:
             all_config.readfp(cfg_file)
 
-        self.goaccess_flag = int(all_config.get('Common', 'goaccess_flag'))
-        self.time_format = all_config.get('Common', 'time-format')
-        self.date_format = all_config.get('Common', 'date-format')
-        self.log_format = all_config.get('Common', 'log-format')
-        self.static_file = all_config.get('Common', 'static-file').split(',')
-        self.is_with_parameters = int(all_config.get('Common', 'is_with_parameters'))
-        self.fixed_parameter_keys = all_config.get('Common', 'fixed_parameter_keys').split(',')
-        self.custom_parameters_list = all_config.get('Common', 'custom_parameters').split(',')
-        self.urls_most_number = int(all_config.get('Common', 'urls_most_number'))
+        self.goaccess_flag = int(all_config.get('common', 'goaccess_flag'))
+        self.time_format = all_config.get('common', 'time-format')
+        self.date_format = all_config.get('common', 'date-format')
+        self.log_format = all_config.get('common', 'log-format')
+        self.static_file = all_config.get('common', 'static-file').split(',')
+        self.is_with_parameters = int(all_config.get('common', 'is_with_parameters'))
+        self.fixed_parameter_keys = all_config.get('common', 'fixed_parameter_keys').split(',')
+        self.custom_parameters_list = all_config.get('common', 'custom_parameters').split(',')
+        self.urls_most_number = int(all_config.get('common', 'urls_most_number'))
 
         self.custom_keys = []
         self.custom_parameters = {}
@@ -32,4 +32,4 @@ class Config():
             self.custom_parameters.setdefault(key, value)
             self.custom_keys.append(key)
 
-config = Config('../conf/Config.ini')
+config = Config('../conf/config.ini')
