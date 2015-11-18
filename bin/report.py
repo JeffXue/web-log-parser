d8556fb175990d3a7ea1b06c4eee6213b1129b26
JeffXue <contactmexkj@163.com>
2015-11-18 11:35:43 +0800

    生成index.html时忽略.gitkeep文件

--- bin/report.py
+++ bin/report.py
@@ -53,7 +53,7 @@ def generate_web_log_parser_urls(data):
 def update_index_html():
     html = index_html_header
     for report_file in sorted(get_dir_files('../result/report/')):
-        if report_file.find('GoAccess') != -1:
+        if report_file.find('GoAccess') != -1 or report_file.find('.git') != -1 :
             pass
         else:
             html += index_html_li % {'web_log_parser_file': report_file}
